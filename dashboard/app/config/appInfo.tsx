1a3e7e2b1d74d80e9972db2cdff9eb30dc603d48
André Roggeri Campos <a.roggeri.c@gmail.com>
2024-07-20 16:53:18 -0300

    chore: Log url

--- dashboard/app/config/appInfo.tsx
+++ dashboard/app/config/appInfo.tsx
@@ -9,6 +9,8 @@ if (!url.includes("http")) {
   url = "https://" + url;
 }
 
+console.log({ url });
+
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: "Playwright Dashboard",
