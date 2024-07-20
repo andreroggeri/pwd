bfe6ef90228bd7e662b4d1c7789778be42527663
André Roggeri Campos <a.roggeri.c@gmail.com>
2024-07-20 16:44:12 -0300

    fix: Url

--- dashboard/app/config/appInfo.tsx
+++ dashboard/app/config/appInfo.tsx
@@ -1,8 +1,14 @@
+let url: string;
+if (typeof window === "undefined") {
+  url = process.env.VERCEL_URL!;
+} else {
+  url = window.location.origin;
+}
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
-  appName: "SuperTokens Remix demo app",
-  apiDomain: "http://localhost:3000",
-  websiteDomain: "http://localhost:3000",
+  appName: "Playwright Dashboard",
+  apiDomain: url,
+  websiteDomain: url,
   apiBasePath: "/supertokens",
   websiteBasePath: "/auth",
 };
